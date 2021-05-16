3f733cc120e5d1990289d9fd1ac5bd0d02671c04
Colin Tseng <klt.aus@yahoo.com>
2021-05-16 12:59:02 +1000

    COmpleted assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render() {
+        if (this.state.secondsLeft!== 0) {
+            
+        return (
+        <div>
+            {console.log(this.state.secondsLeft)}
+            {this.state.secondsLeft} seconds left before I go boom!
+        </div>
+        )
+        } else {
+            return (
+                <div>
+                Boom!
+            </div>
+            )
+        }
+    }
+}
